7f4694eb4c151eed5a8253e96302390867af252d
xadija12345678 <etec02m230485@epu.edu.iq>
2025-04-23 21:08:45 -0700

    numbertwocommit

---
+++ CarShowroomSystem/model/Admin.java
@@ -0,0 +1,9 @@
+public class Admin extends User {
+    public Admin(String username, String password) {
+    super(username, password);
+    }
+    @Override
+    public String getRole() {
+    return "Admin";
+    }
+   }
