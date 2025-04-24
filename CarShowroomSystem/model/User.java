db06c54fadcf2676b7d1223ffa1ef2de10189cd8
xadija12345678 <etec02m230485@epu.edu.iq>
2025-04-23 21:10:12 -0700

    third

--- CarShowroomSystem/model/User.java
+++ CarShowroomSystem/model/User.java
@@ -1,20 +1,13 @@
-package model ; 
-public class User { 
-private int id; 
-private String username; 
-private String password; 
-private String role; // admin or customer 
-public User(int id, String username, String password, String role) { 
-this.id = id; 
-this.username = username; 
-this.password = password; 
-this.role = role; 
-} 
-public int getId() { return id; } 
-public String getUsername() { return username; } 
-public String getPassword() { return password; } 
-public String getRole() { return role; } 
-public void setUsername(String username) { this.username = username; } 
-public void setPassword(String password) { this.password = password; } 
-public void setRole(String role) { this.role = role; } 
-}
+public abstract class User {
+    protected String username;
+    protected String password;
+    public User(String username, String password) {
+    this.username = username;
+    this.password = password;
+    }
+    public String getUsername() {
+    return username;
+    }
+    public abstract String getRole();
+   }
+   
