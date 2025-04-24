2e3df3b0c67316f940f252cc2a552ee2c78cfb7d
xadija12345678 <etec02m230485@epu.edu.iq>
2025-04-23 21:32:36 -0700

    mm

--- CarShowroomSystem/Main.java
+++
@@ -1,8 +0,0 @@
-import view.LoginFrame; 
-public class Main { 
-public static void main(String[] args) { 
-javax.swing.SwingUtilities.invokeLater(() -> { 
-new LoginFrame().setVisible(true); 
-}); 
-} 
-}
