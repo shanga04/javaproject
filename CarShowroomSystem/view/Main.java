597771fbd2ed0faa3141e630c0664bc59a7f7b2d
farek04 <etec02m230523@epu.edu.iq>
2025-04-30 23:18:32 +0300

    delet

--- CarShowroomSystem/view/Main.java
+++
@@ -1,9 +0,0 @@
-import util.MessageBox;
-import view.LoginFrame; 
-public class Main { 
-public static void main(String[] args) {
-     javax.swing.SwingUtilities.invokeLater(() -> { new UserRoleSelector().setVisible(true);
-new LoginFrame().setVisible(true); 
-}); 
-} 
-} 
