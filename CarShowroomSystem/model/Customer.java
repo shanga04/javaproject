30bedae77f7e926bf26fec43ce1b03759910af32
farek04 <etec02m230523@epu.edu.iq>
2025-04-30 23:20:15 +0300

    delet2

--- CarShowroomSystem/model/Customer.java
+++
@@ -1,9 +0,0 @@
-public class Customer extends User {
-    public Customer(String username, String password) {
-    super(username, password);
-    }
-    @Override
-    public String getRole() {
-    return "Customer";
-    }
-   }
